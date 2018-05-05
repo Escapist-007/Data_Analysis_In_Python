aadeb944b2845529f0e591bb353c1171eb4bda8a
Md Moniruzzaman <38385928+Escapist-007@users.noreply.github.com>
2018-05-06 03:46:17 +0600

    Remove extra comments

--- Q&A series from Data School/03. Methods vs Attributes in Dataframe.py
+++ Q&A series from Data School/03. Methods vs Attributes in Dataframe.py
@@ -1,12 +1,4 @@
 
-# coding: utf-8
-
-# ### <font color=blue> Ques 03: </font>  <font color=red>  Why do some pandas commands end with parentheses, and other commands not? </font> 
-
-# >**Method** ends with **parenthesis** but **attribute** doesn't.
-
-# In[2]:
-
 
 import pandas as pd
 
@@ -15,27 +7,9 @@ movie_ratings_ds = pd.read_csv('http://bit.ly/imdbratings')
 movie_ratings_ds.head(4)
 
 
-# In[3]:
-
-
-# Give some descriptive statistical info about the numerical columns (default) of the dataframe
-movie_ratings_ds.describe()  
-
-
-# In[4]:
-
-
+movie_ratings_ds.describe()  # Give some descriptive statistical info about the numerical columns (default) of the dataframe
 movie_ratings_ds.shape       # Returns no. of rows, no. of columns in the Dataframe
-
-
-# In[5]:
-
-
-movie_ratings_ds.dtypes     # Show the datatype of all columns
-
-
-# In[6]:
-
+movie_ratings_ds.dtypes      # Show the datatype of all columns
 
 movie_ratings_ds.describe(include=['object']) 
 # top --> Record having highest frequency , freq --> No. of frequecny of the top record
