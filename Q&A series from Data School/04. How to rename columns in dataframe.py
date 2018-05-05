72acb6a186bac86d4dbf0b451e686fa117e86f43
Md Moniruzzaman <38385928+Escapist-007@users.noreply.github.com>
2018-05-06 03:58:24 +0600

    Remove extra comments

--- Q&A series from Data School/04. How to rename columns in dataframe.py
+++ Q&A series from Data School/04. How to rename columns in dataframe.py
@@ -1,11 +1,3 @@
-
-# coding: utf-8
-
-# ### <font color=blue> Ques 04: </font>  <font color=red> How do I `rename` columns in pandas dataframe? </font>
-
-# In[2]:
-
-
 import pandas as pd
 
 # See the column names
@@ -13,36 +5,17 @@ ufo_ds = pd.read_csv('http://bit.ly/uforeports')
 ufo_ds.columns
 
 
-# In[3]:
-
-
 ufo_ds.head(3)
-
-
-# In[4]:
-
 
 ufo_ds.rename(columns = {'Colors Reported':'Color_Reported', 'Shape Reported':'Shape_Reported'})
 ufo_ds.columns
 
-
-# In[5]:
-
-
 # The column name is not changed permanently. We have to put one more argument : inplace = True.
-
 ufo_ds.rename(columns = {'Colors Reported':'Color_Reported', 'Shape Reported':'Shape_Reported'}, inplace=True)
 ufo_ds.columns
 
 
-# In[6]:
-
-
 ufo_ds.head(2)
-
-
-# In[8]:
-
 
 # Another way of renaming  [ If we want to change all the column name ]
 
@@ -50,8 +23,6 @@ ufo_col = ['city', 'color', 'shape', 'state', 'time']
 ufo_ds.columns = ufo_col
 ufo_ds.columns
 
-
-# In[9]:
 
 
 # Or we can rename the columns name when we load the dataset
@@ -62,25 +33,13 @@ ufo_ds = pd.read_csv('http://bit.ly/uforeports', header=0, names = ufo_col)  # R
 ufo_ds.head(3)
 
 
-# > ### If we have 100 columns having `space in their names`, and we want to replace these spaces. How we can do that ? 
-
-# We can do that using **string methods**. Each column name is a string
-
-# In[10]:
-
+# If we have 100 columns having `space in their names`, and we want to replace these spaces. How we can do that ? 
+# We can do that using string methods. Each column name is a string
 
 ufo_ds = pd.read_csv('http://bit.ly/uforeports')
 ufo_ds.head(2)
 
 
-# In[11]:
-
-
 ufo_ds.columns = ufo_ds.columns.str.replace(' ','_')  # "ufo_ds.columns" is reassigned
-
-
-# In[12]:
-
-
 ufo_ds.head(3)
 
